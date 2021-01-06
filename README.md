d5d838236cee0d01221df98f1ce1a33429d297b8
Praneet Nigam <pnigam@pokerbaazi.in>
2021-01-06 21:28:54 +0530

    new site

--- README.md
+++
@@ -1,1 +0,0 @@
-## Design static website 
---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000 );
+    });
+});
+
+$(document).ready(function() {
+  $('#autoWidth').lightSlider({
+      autoWidth:true,
+      loop:true,
+      onSliderLoad: function() {
+          $('#autoWidth').removeClass('cS-hidden');
+      } 
+  });  
+});
+
+$(document).ready(function() {
+  $('#autoWidth2').lightSlider({
+      autoWidth:true,
+      loop:true,
+      onSliderLoad: function() {
+          $('#autoWidth').removeClass('cS-hidden');
+      } 
+  });  
+});
